59f649148322fd1cf24865d82fa1797c3c4f603e
Omkar Kadam <omkarkadam1115@gmail.com>
2024-12-13 22:36:43 +0530

    update javascript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello omkar");
+function togglePassword() {
+    const passwordInput = document.getElementById('password');
+    const toggleIcon = document.getElementById('toggleIcon');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleIcon.src = 'https://img.icons8.com/ios-filled/50/000000/invisible.png';
+    } else {
+        passwordInput.type = 'password';
+        toggleIcon.src = 'https://img.icons8.com/ios-filled/50/000000/visible.png';
+    }
+}
